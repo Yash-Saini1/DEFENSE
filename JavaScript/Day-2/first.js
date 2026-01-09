b0e2a02c1bc00e78f738b9855085367e7a85fe37
Yash Saini <yashsaini10@outlook.in>
2026-01-09 23:29:19 +0530

    first.js

---
+++ JavaScript/Day-2/first.js
@@ -0,0 +1,17 @@
+// Variable Declaration
+
+let name = "Rohit";
+let age = 20;
+
+age = 30;
+
+console.log(name, age);
+
+const account = 1234;
+console.log(account); 
+
+/* Old Method */
+
+var a = 10;
+a = 20;
+console.log(a);
