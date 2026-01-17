36cafb256e1fe6ed1d97d534e2b33219565518d8
Yash Saini <yashsaini10@outlook.in>
2026-01-17 21:14:38 +0530

    math.js

--- JavaScript/Day-5/math.js
+++ JavaScript/Day-5/math.js
@@ -1,1 +1,35 @@
+console.log(Math.abs(-4));
 
+console.log(Math.PI);
+
+console.log(Math.LN10);
+
+console.log(Math.SQRT2);
+
+console.log(Math.ceil(6.3));
+
+console.log(Math.floor(6.3));
+
+console.log(Math.log10(20));
+
+console.log(Math.max(20,11,34,21));
+
+console.log(Math.min(20,11,34,21));
+
+console.log(Math.random());
+
+// Generating Random Numbers from 0 to 9
+console.log(Math.floor(Math.random()*10));
+
+// Generating Random Numbers from 1 to 10
+console.log(Math.floor(Math.random()*10)+1);
+
+// Generating Random Numbers from 1 to 6
+console.log(Math.floor(Math.random()*6)+1);
+
+// Generating Random Numbers from 15 to 25
+console.log(Math.floor(Math.random()*11)+15);
+
+// Generate a 4 digit OTP
+
+console.log(Math.floor(Math.random()*(9999-1000+1))+1000); 
